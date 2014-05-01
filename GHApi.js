4dbd56a7b19b3917f51b5fc4928e3a5ad1ff09a0
Kado Damball <kadodamball@hotmail.com>
2014-05-01 18:03:09 +0300

    Solved the Async problems

--- GHApi.js
+++ GHApi.js
@@ -1,34 +1,32 @@
 /*
-  Here's the problem. I can make this work fine. However:
-  I need to find a way to ensure that when getUser() is called, only then can it fire
-  the ajax request. However, this means it will take time to return the result.
-  Currently it either fires both requests (ie getUser & getRepo) or it tries to parse the response before ajax
-  has received anything. I'm trying to find an optimal point where only the specific 
-  ajax request is fired AND we parse the response at the right time.
-  
-  ALSO, I can easily solve this using jQuery but we dont need anymore jQUERY!!!
+ - Breakthrough
+ - Now we only need to process the data received!
 */
 
 var GH = (function(details){
   var user, repo, org,
-    connectToAPI;
+    connectToAPI, successHandler;
+  
+  successHandler = function(data){
+    return data ? console.log(JSON.parse(data)) : console.log("Something went wrong...call the GhostBusters");
+  };
   
   connectToAPI = function(req){
     var xhrToGH = new XMLHttpRequest();
-    xhrToGH.open("get", req, "open");
+    xhrToGH.open("get", req, false);
     xhrToGH.send();
-    return xhrToGH;
+    xhrToGH.onload = successHandler(xhrToGH.response);
   };
   
   user = function(){
     var url = "https://api.github.com/users/" + details.user;
-    return details.user ? connectToAPI(url) : console.log("Error, you need to provide a Github Username");
+    details.user ? connectToAPI(url) : console.log("Error, you need to provide a Github Username");
   };
   
   repo = function(){
     var url = "https://api.github.com/repos/" + [details.user, details.repo].join("/");
-    return details.repo ? connectToAPI(url) : console.log("Error, please provide a repo name!");
-  };
+    details.repo ? connectToAPI(url) : console.log("Error, please provide a repo name!");
+  }; 
   
   org = function(){
     console.log("Sorry, Organization info isn't available yet. I'm working on it!");
