c368c182aeac92d3d9704385b1c6aa6c4ce5ab45
Kado Damball <kadodamball@hotmail.com>
2014-05-01 23:19:52 +0300

    Cleaning up the returned args

--- js/GHApi.js
+++ js/GHApi.js
@@ -4,29 +4,30 @@
 */
 
 var GH = (function(details){
-  var user, repo, org,
+  var user, repo, org, events,
     connectToAPI, successHandler;
-  
-  successHandler = function(data){
-    return data ? JSON.parse(data) : console.log("Something went wrong...call the GhostBusters");
-  };
   
   connectToAPI = function(req){
     var xhrToGH = new XMLHttpRequest();
     xhrToGH.open("get", req, false);
     xhrToGH.send();
-    xhrToGH.onload = successHandler(xhrToGH.response);
+    return xhrToGH.onload = function(){return xhrToGH.response};
   };
   
   user = function(){
     var url = "https://api.github.com/users/" + details.user;
-    details.user ? connectToAPI(url) : console.log("Error, you need to provide a Github Username");
+    return details.user ? connectToAPI(url)() : console.log("Error, you need to provide a Github Username");
   };
   
   repo = function(){
     var url = "https://api.github.com/repos/" + [details.user, details.repo].join("/");
-    details.repo ? connectToAPI(url) : console.log("Error, please provide a repo name!");
-  }; 
+    return details.repo && details.user ? connectToAPI(url)() : console.log("Error, please provide a repo & username!");
+  };
+  
+  events = function(){
+    var url = "https://api.github.com/users/" + [details.user, "events"].join("/");
+    return details.events && details.user ? connectToAPI(url)() : console.log("Error, please provide username and/or confirm events!");
+  };
   
   org = function(){
     console.log("Sorry, Organization info isn't available yet. I'm working on it!");
@@ -35,6 +36,6 @@ var GH = (function(details){
   return {
     getUser: user,
     getRepo: repo,
-    getOrganization: org
+    getEvents: events
   };
 });
