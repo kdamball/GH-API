4f5b0903f8e4d76c2c5e13c9386b0a14d1abd9c9
Kado Damball <kadodamball@hotmail.com>
2014-05-13 18:52:48 +0300

    Clearing Organizational repo errors

--- js/GHApi.js
+++ js/GHApi.js
@@ -24,20 +24,21 @@ var GH = (function(details){
     return details.user ? successHandler(connectToAPI(url)) : console.log("Error, you need to provide a Github Username");
   };
   
-  repo = function(){
-    var url = "https://api.github.com/repos/" + [details.user, details.repo].join("/");
-    return details.repo && details.user ? successHandler(connectToAPI(url)) : console.log("Error, please provide a repo & username!");
-  };
-  
-  events = function(){
-    var url = "https://api.github.com/users/" + [details.user, "events"].join("/");
-    return details.events && details.user ? successHandler(connectToAPI(url)) : console.log("Error, please provide username and/or confirm events!");
-  };
-  
   org = function(){
     var url = "https://api.github.com/orgs/" + details.organization;
     return details.organization ? successHandler(connectToAPI(url)) : console.log("Error, please provide an organization name");
   };
+  
+  repo = function(){
+    var url = "https://api.github.com/repos/" + [details.user || details.organization, details.repo].join("/");
+    return details.repo && (details.user || details.organization) ? successHandler(connectToAPI(url)) : console.log("Error, please provide a repo & username/organization!");
+  };
+  
+  events = function(){
+    var url = "https://api.github.com/users/" + [details.user || details.organization, "events"].join("/");
+    return details.events && (details.user || details.organization) ? successHandler(connectToAPI(url)) : console.log("Error, please provide username/organization and/or confirm events!");
+  };
+  
   
   return {
     getUser: user,
