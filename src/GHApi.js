6b0000d8abe21d1d3f83d70f257889d9523981d1
Kado Damball <kadodamball@hotmail.com>
2014-07-16 14:28:52 +0300

    Simplified the error handler

--- src/GHApi.js
+++ src/GHApi.js
@@ -12,8 +12,8 @@ var GH = (function(details){
     return JSON.parse(success);
   };
   
-  errorHandler = function(id, extra){
-    throw new Error("Please provide either the user or repo's name and/or confirm events");
+  errorHandler = function(){
+    throw new Error("Please provide either the user, repo's name and/or confirm events");
   }
   
   connectToAPI = function(req){
@@ -27,22 +27,22 @@ var GH = (function(details){
   
   user = function(){
     var url = "https://api.github.com/users/" + details.user;
-    return details.user ? successHandler(connectToAPI(url)) : errorHandler(0);
+    return details.user ? successHandler(connectToAPI(url)) : errorHandler();
   };
   
   org = function(){
     var url = "https://api.github.com/orgs/" + details.organization;
-    return details.organization ? successHandler(connectToAPI(url)) : errorHandler(1);
+    return details.organization ? successHandler(connectToAPI(url)) : errorHandler();
   };
   
   repo = function(){
     var url = "https://api.github.com/repos/" + [details.user || details.organization, details.repo].join("/");
-    return details.repo && (details.user || details.organization) ? successHandler(connectToAPI(url)) : errorHandler(0, 2);
+    return details.repo && (details.user || details.organization) ? successHandler(connectToAPI(url)) : errorHandler();
   };
   
   events = function(){
     var url = "https://api.github.com/users/" + [details.user || details.organization, "events"].join("/");
-    return details.events && (details.user || details.organization) ? successHandler(connectToAPI(url)) : errorHandler(0, 3);
+    return details.events && (details.user || details.organization) ? successHandler(connectToAPI(url)) : errorHandler();
   };
   
   
